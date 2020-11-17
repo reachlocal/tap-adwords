f8e57f3754193de0fab04f4a5131e6b2b3af88ce
Georgi Yolovski <georgi.yolovski@accedia.com>
2020-11-17 16:09:39 +0200

    Added report retrieval. Mapped ad performance report.

--- tap_adwords/__init__.py
+++ tap_adwords/__init__.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import os
-import json
+import simplejson as json
 import singer
 from singer import utils, metadata
 from singer.catalog import Catalog, CatalogEntry
 from singer.schema import Schema
+import requests
+from datetime import datetime
 
 
-REQUIRED_CONFIG_KEYS = ["start_date", "username", "password"]
+REQUIRED_CONFIG_KEYS = []
 LOGGER = singer.get_logger()
 
 
@@ -58,34 +60,50 @@ def sync(config, state, catalog):
     for stream in catalog.get_selected_streams(state):
         LOGGER.info("Syncing stream:" + stream.tap_stream_id)
 
-        bookmark_column = stream.replication_key
-        is_sorted = True  # TODO: indicate whether data is sorted ascending on bookmark value
-
+        schema = utils.load_json(get_abs_path("schemas/{}.json".format(stream.tap_stream_id)))
         singer.write_schema(
             stream_name=stream.tap_stream_id,
-            schema=stream.schema,
+            schema=schema,
             key_properties=stream.key_properties,
         )
 
-        # TODO: delete and replace this inline function with your own data retrieval process:
-        tap_data = lambda: [{"id": x, "name": "row${x}"} for x in range(1000)]
+        data = get_report(stream.tap_stream_id, config, schema)
 
-        max_bookmark = None
-        for row in tap_data():
-            # TODO: place type conversions or transformations here
-
-            # write one or more rows to the stream:
+        for row in data:
             singer.write_records(stream.tap_stream_id, [row])
-            if bookmark_column:
-                if is_sorted:
-                    # update bookmark to latest value
-                    singer.write_state({stream.tap_stream_id: row[bookmark_column]})
-                else:
-                    # if data unsorted, save max value until end of writes
-                    max_bookmark = max(max_bookmark, row[bookmark_column])
-        if bookmark_column and not is_sorted:
-            singer.write_state({stream.tap_stream_id: max_bookmark})
+        singer.write_state({"last_updated_at": datetime.now().isoformat()})
     return
+
+def get_report(stream, config, schema):
+    fields = list(schema["properties"].keys())
+    url = "https://adwords.google.com/api/adwords/reportdownload/v201809"
+
+    payload={
+        '__rdquery': f'SELECT {", ".join(fields)} FROM {stream} DURING LAST_30_DAYS',
+        '__fmt': 'CSV'}
+    files=[
+
+    ]
+    headers = {
+        'developerToken': config["developerToken"],
+        'Authorization': f'Bearer {config["accessToken"]}',
+        'clientCustomerId': config["customerId"],
+        'skipReportHeader': "true",
+        'skipReportSummary': "true",
+        'skipColumnHeader': "true"
+    }
+
+    resp = requests.post(url, headers=headers, data=payload, files=files)
+    lines = resp.text.splitlines(False)
+    result = []
+    for line in lines:
+        items = line.split(',')
+        obj = {}
+        for index in range(len(items)):
+            obj[fields[index]] = items[index]
+        result.append(obj)
+
+    return result
 
 
 @utils.handle_top_exception(LOGGER)
